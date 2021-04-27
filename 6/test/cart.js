0a7be6c462e27d88602746675e1ae8e8a5c3e223
Artem <gaidash2303@gmail.com>
2021-04-27 20:12:43 +0300

    add homework 6

--- 6/test/cart.js
+++ 6/test/cart.js
@@ -1,33 +1,3 @@
-
-var link = document.querySelectorAll(".product-flex a");
-var img = document.querySelectorAll(".product-flex img");
-
-link[0].addEventListener("click", f2);
-link[1].addEventListener("click", f1);
-
-for (i = 1; i < img.length; i++) {
-    img[i].style.display = "none";
-}
-
-var i = 0;
-function f1() {
-    img[i].style.display = "none";
-    i++;
-    if (i >= img.length) {
-        i = 0;
-    }
-    img[i].style.display = "block";
-}
-
-function f2() {
-    img[i].style.display = "none";
-    i -= 1;
-    if (i < 0) {
-        i = img.length - 1;
-    }
-    img[i].style.display = "block";
-}
-
 var itemscheck = document.querySelectorAll(".items-list .items-cards");
 
 var items = [];
